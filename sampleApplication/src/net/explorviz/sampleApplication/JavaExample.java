f8b3b5cb5314eaf00469aeba2ae9cbd43b2ec46e
christian <czi@informatik.uni-kiel.de>
2017-12-20 15:02:07 +0100

    Modified sampleApplication

---
+++ sampleApplication/src/net/explorviz/sampleApplication/JavaExample.java
@@ -0,0 +1,32 @@
+package net.explorviz.sampleApplication;
+
+import java.util.Scanner;
+import java.util.logging.Logger;
+
+/**
+ * JDBC example application class for testing the usual instrumentation aspects
+ * 
+ * @author Christian Zirkelbach (czi@informatik.uni-kiel.de)
+ *
+ */
+public class JavaExample {
+
+	private static final Logger LOG = Logger.getLogger(JavaExample.class.getName());
+
+	public static void main(String[] args) {
+		LOG.info("Hello, I'm a fibunacci calculator!");
+		LOG.info("Enter number upto which Fibonacci series to print: ");
+		Scanner scanner = new Scanner(System.in);
+		int number = scanner.nextInt();
+		LOG.info(String.valueOf(fibonacci(number)));
+		scanner.close();
+	}
+
+	public static int fibonacci(int number) {
+		if (number == 1 || number == 2) {
+			return 1;
+		}
+		return fibonacci(number - 1) + fibonacci(number - 2);
+	}
+
+}
---
+++ sampleApplication/src/net/explorviz/sampleApplication/Main.java
@@ -0,0 +1,13 @@
+package net.explorviz.sampleApplication;
+
+/**
+ * Sample Application for testing the monitoring component (Kieker) for ExplorViz
+ * @author Christian Zirkelbach (czi@informatik.uni-kiel.de)
+ *
+ */
+public class Main {
+	public static void main(String[] args) {
+		JavaExample.main(args);
+		JDBCExample.main(args);
+	}
+}
