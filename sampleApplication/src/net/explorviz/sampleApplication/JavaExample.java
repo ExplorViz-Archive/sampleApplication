5142a872a22a83358ea923a3275bde0e854b974e
christian <czi@informatik.uni-kiel.de>
2017-12-20 16:37:17 +0100

    Fixed monitoring package path

--- sampleApplication/src/net/explorviz/sampleApplication/JavaExample.java
+++ sampleApplication/src/net/explorviz/sampleApplication/JavaExample.java
@@ -14,7 +14,7 @@ public class JavaExample {
 	private static final Logger LOG = Logger.getLogger(JavaExample.class.getName());
 
 	public static void main(String[] args) {
-		if (args == null || args.length == 0) {
+		if (args != null && args.length == 1) {
 			LOG.info("Hello, I'm a fibunacci calculator!");
 			LOG.info("Enter number upto which Fibonacci series to print: ");
 			Scanner scanner = new Scanner(System.in);
@@ -22,7 +22,7 @@ public class JavaExample {
 			LOG.info(String.valueOf(fibonacci(number)));
 			scanner.close();
 		} else {
-			fibonacci(Integer.valueOf(args[0]));
+			fibonacci(Integer.valueOf(25));
 		}
 	}
 
