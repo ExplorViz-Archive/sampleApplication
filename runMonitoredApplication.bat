5ae6d6da3f602827de5472803059cce8a8f516d2
Christian Zirkelbach <czi@informatik.uni-kiel.de>
2018-02-02 13:22:40 +0100

    Added application naming and automatic system monitoring
    1. The application name can now be configured in the kieker.properties
    2. Automatic system/host monitoring is applied from now on via Kieker (CPU Utilization, Memory Usage)
    3. 2. needs a large part of kieker-binaries to work the moment. This could be much easier, when https://kieker-monitoring.atlassian.net/browse/KIEKER-1690 is implemented.

--- runMonitoredApplication.bat
+++ runMonitoredApplication.bat
@@ -1,2 +1,10 @@
 @ECHO OFF
+
+REM Get the directory of this file and change the working directory to it.
+cd %~dp0
+
+REM Set every variable we will need for the execution.
+SET BINDIR=%cd%
+
+START "kiekerSystemMonitoring" ".\kiekerSystemMonitoring\kieker-1.14-SNAPSHOT\bin\resourceMonitor.bat"
 java -javaagent:kieker-1.14-SNAPSHOT-aspectj.jar -cp . -jar sampleApplication.jar
--- runMonitoredApplication.sh
+++ runMonitoredApplication.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
+./kiekerSystemMonitoring/kieker-1.14-SNAPSHOT/bin/resourceMonitor.sh &
 java -javaagent:kieker-1.14-SNAPSHOT-aspectj.jar -cp . -jar sampleApplication.jar
+
