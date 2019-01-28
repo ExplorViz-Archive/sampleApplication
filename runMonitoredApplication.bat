da50ca316295e8f6f49d8299749b8e3c535af9f7
Christian Zirkelbach <czi@informatik.uni-kiel.de>
2019-01-28 09:05:58 +0100

    Changed logged timeunit in Kieker to milliseconds

--- runMonitoredApplication.bat
+++ runMonitoredApplication.bat
@@ -1,3 +1,8 @@
+:: ==============================================================
+:: Script for starting the monitoring component for ExplorViz
+::
+:: Christian Zirkelbach (czi@informatik.uni-kiel.de)
+:: ==============================================================
 @ECHO OFF
 
 REM Get the directory of this file and change the working directory to it.
@@ -7,4 +12,4 @@ REM Set every variable we will need for the execution.
 SET BINDIR=%cd%
 
 START "kiekerSystemMonitoring" ".\kiekerSystemMonitoring\kieker-1.14-SNAPSHOT\bin\resourceMonitor.bat"
-java -javaagent:kieker-1.14-SNAPSHOT-aspectj.jar -Dkieker.monitoring.skipDefaultAOPConfiguration=true -cp . -jar sampleApplication.jar
+java -javaagent:kieker-1.14-SNAPSHOT-aspectj.jar -Dkieker.monitoring.configuration=.\META-INF\kieker.monitoring.properties -Dorg.aspectj.weaver.loadtime.configuration=.\META-INF\aop.xml -cp . -jar sampleApplication.jar
--- runMonitoredApplication.sh
+++ runMonitoredApplication.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+#  ==============================================================
+# Script for starting the monitoring component for ExplorViz
+#
+# Christian Zirkelbach (czi@informatik.uni-kiel.de)
+# ==============================================================
 ./kiekerSystemMonitoring/kieker-1.14-SNAPSHOT/bin/resourceMonitor.sh &
-java -javaagent:kieker-1.14-SNAPSHOT-aspectj.jar -Dkieker.monitoring.skipDefaultAOPConfiguration=true -cp . -jar sampleApplication.jar
+java -javaagent:kieker-1.14-SNAPSHOT-aspectj.jar -Dkieker.monitoring.configuration=./META-INF/kieker.monitoring.properties -Dorg.aspectj.weaver.loadtime.configuration=./META-INF/aop.xml -cp . -jar sampleApplication.jar
 
