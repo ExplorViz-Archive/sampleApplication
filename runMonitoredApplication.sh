f709b7f921d662fba81b75f34a1c74b7d09f01cc
Jan Erik Petersen <JanErikPetersen@web.de>
2019-07-18 16:45:23 +0200

    Fix UNIX scripts, update for remoteSampleApplication
    - Port changes to runMonitoredApplication.bat to .sh variant
    - Add a 3 second sleep after starting the server to prevent first client crashing
    - Add executable flag to resourceMonitor.sh (needed to work on Linux)

--- runMonitoredApplication.sh
+++ runMonitoredApplication.sh
@@ -1,9 +1,21 @@
 #!/bin/sh
-#  ==============================================================
+# ==============================================================
 # Script for starting the monitoring component for ExplorViz
 #
 # Christian Zirkelbach (czi@informatik.uni-kiel.de)
 # ==============================================================
-./kiekerSystemMonitoring/kieker-1.14-SNAPSHOT/bin/resourceMonitor.sh &
-java -javaagent:kieker-1.14-SNAPSHOT-aspectj.jar -Dkieker.monitoring.configuration=./META-INF/kieker.monitoring.properties -Dorg.aspectj.weaver.loadtime.configuration=./META-INF/aop.xml -cp . -jar sampleApplication.jar
 
+# When this script exits (e.g. due to Ctrl-C) kill all our children as well
+trap "trap - TERM && kill 0" INT TERM EXIT
+
+./kiekerSystemMonitoring/kieker-1.14-SNAPSHOT/bin/resourceMonitor.sh &
+java -javaagent:kieker-1.14-SNAPSHOT-aspectj.jar -Dkieker.monitoring.configuration=./META-INF/kieker.monitoring.sampleApplication.properties -Dorg.aspectj.weaver.loadtime.configuration=./META-INF/aop.sampleApplication.xml -cp . -jar sampleApplication.jar &
+java -javaagent:kieker-1.14-SNAPSHOT-aspectj.jar -Dkieker.monitoring.configuration=./META-INF/kieker.monitoring.remoteSampleApplicationServer.properties -Dorg.aspectj.weaver.loadtime.configuration=./META-INF/aop.remoteSampleApplicationServer.xml -cp . -jar remoteSampleApplicationServer.jar &
+
+# Allow server some time to start before starting clients
+sleep 3
+
+while true; do
+    java -javaagent:kieker-1.14-SNAPSHOT-aspectj.jar -Dkieker.monitoring.configuration=./META-INF/kieker.monitoring.remoteSampleApplicationClient.properties -Dorg.aspectj.weaver.loadtime.configuration=./META-INF/aop.remoteSampleApplicationClient.xml -cp . -jar remoteSampleApplicationClient.jar &
+    sleep 5
+done
